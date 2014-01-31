f00fa52e5f1682a7e34f06cdf9e461952ee336ff
Damian Galarza and Harry Schwartz <support+Damian+Harry@thoughtbot.com>
2014-01-31 14:46:39 -0500

    Clean up specs based on PR feedback

--- spec/shoulda/matchers/action_controller/strong_parameters_matcher_spec.rb
+++ spec/shoulda/matchers/action_controller/strong_parameters_matcher_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Shoulda::Matchers::ActionController do
-  describe ".permit" do
-    it "is true when the sent parameter is allowed" do
+  describe "#permit" do
+    it 'matches when the sent parameter is allowed' do
       controller_class = controller_for_resource_with_strong_parameters do
         params.require(:user).permit(:name)
       end
@@ -10,7 +10,7 @@ describe Shoulda::Matchers::ActionController do
       expect(controller_class).to permit(:name).for(:create)
     end
 
-    it "is false when the sent parameter is not allowed" do
+    it 'does not match when the sent parameter is not allowed' do
       controller_class = controller_for_resource_with_strong_parameters do
         params.require(:user).permit(:name)
       end
@@ -18,7 +18,7 @@ describe Shoulda::Matchers::ActionController do
       expect(controller_class).not_to permit(:admin).for(:create)
     end
 
-    it "allows multiple attributes" do
+    it 'matches against multiple attributes' do
       controller_class = controller_for_resource_with_strong_parameters do
         params.require(:user).permit(:name, :age)
       end
@@ -37,33 +37,33 @@ describe Shoulda::Matchers::ActionController::StrongParametersMatcher do
 
   describe "#matches?" do
     it "is true for a subset of the allowable attributes" do
-      matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, self).for(:create)
+      matcher = described_class.new(:name, self).for(:create)
       expect(matcher.matches?).to be_true
     end
 
     it "is true for all the allowable attributes" do
-      matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, :age, self).for(:create)
+      matcher = described_class.new(:name, :age, self).for(:create)
       expect(matcher.matches?).to be_true
     end
 
     it "is false when any attributes are not allowed" do
-      matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, :admin, self).for(:create)
+      matcher = described_class.new(:name, :admin, self).for(:create)
       expect(matcher.matches?).to be_false
     end
 
     it "is false when permit is not called" do
-      matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, self).for(:new, verb: :get)
+      matcher = described_class.new(:name, self).for(:new, verb: :get)
       expect(matcher.matches?).to be_false
     end
 
     it "requires an action" do
-      matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, self)
+      matcher = described_class.new(:name, self)
       expect { matcher.matches? }
         .to raise_error(Shoulda::Matchers::ActionController::StrongParametersMatcher::ActionNotDefinedError)
     end
 
     it "requires a verb for non-restful action" do
-      matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, self).for(:authorize)
+      matcher = described_class.new(:name, self).for(:authorize)
       expect { matcher.matches? }
         .to raise_error(Shoulda::Matchers::ActionController::StrongParametersMatcher::VerbNotDefinedError)
     end
@@ -71,19 +71,19 @@ describe Shoulda::Matchers::ActionController::StrongParametersMatcher do
 
   describe "#does_not_match?" do
     it "it is true if any of the given attributes are allowed" do
-      matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, :admin, self).for(:create)
+      matcher = described_class.new(:name, :admin, self).for(:create)
       expect(matcher.does_not_match?).to be_true
     end
 
     it "it is false if all of the given attribtues are allowed" do
-      matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, :age, self).for(:create)
+      matcher = described_class.new(:name, :age, self).for(:create)
       expect(matcher.does_not_match?).to be_false
     end
   end
 
   describe "#failure_message" do
     it "includes all missing attributes" do
-      matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, :age, :city, :country, self).for(:create)
+      matcher = described_class.new(:name, :age, :city, :country, self).for(:create)
       matcher.matches?
 
       expect(matcher.failure_message).to eq "Expected controller to permit city and country, but it did not."
@@ -92,7 +92,7 @@ describe Shoulda::Matchers::ActionController::StrongParametersMatcher do
 
   describe "#negative_failure_message" do
     it "includes all attributes that should not have been allowed but were" do
-      matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, :age, :city, :country, self).for(:create)
+      matcher = described_class.new(:name, :age, :city, :country, self).for(:create)
       expect(matcher.does_not_match?).to be_true
 
       expect(matcher.negative_failure_message).to eq "Expected controller not to permit city and country, but it did."
@@ -103,7 +103,7 @@ describe Shoulda::Matchers::ActionController::StrongParametersMatcher do
     context "when given :create" do
       it "posts to the controller" do
         context = stub('context', post: nil)
-        matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, context).for(:create)
+        matcher = described_class.new(:name, context).for(:create)
 
         matcher.matches?
         expect(context).to have_received(:post).with(:create)
@@ -113,7 +113,7 @@ describe Shoulda::Matchers::ActionController::StrongParametersMatcher do
     context "when given :update" do
       it "puts to the controller" do
         context = stub('context', put: nil)
-        matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, context).for(:update)
+        matcher = described_class.new(:name, context).for(:update)
 
         matcher.matches?
         expect(context).to have_received(:put).with(:update)
@@ -123,7 +123,7 @@ describe Shoulda::Matchers::ActionController::StrongParametersMatcher do
     context "when given a custom action and verb" do
       it "puts to the controller" do
         context = stub('context', delete: nil)
-        matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, context).for(:hide, verb: :delete)
+        matcher = described_class.new(:name, context).for(:hide, verb: :delete)
 
         matcher.matches?
         expect(context).to have_received(:delete).with(:hide)
@@ -134,7 +134,7 @@ describe Shoulda::Matchers::ActionController::StrongParametersMatcher do
   describe "#in_context" do
     it 'sets the object the controller action is sent to' do
       context = stub('context', post: nil)
-      matcher = Shoulda::Matchers::ActionController::StrongParametersMatcher.new(:name, nil).for(:create).in_context(context)
+      matcher = described_class.new(:name, nil).for(:create).in_context(context)
 
       matcher.matches?
 
